fde9b8731fc5c0389cfa18cd166046a3012cfcb1
JanMalch <25508038+JanMalch@users.noreply.github.com>
2022-08-16 17:45:24 +0200

    refactor: use immutable lists

--- app/src/main/java/io/github/janmalch/pocpic/data/SourceFactoryConfigRepository.kt
+++ app/src/main/java/io/github/janmalch/pocpic/data/SourceFactoryConfigRepository.kt
@@ -2,15 +2,17 @@ package io.github.janmalch.pocpic.data
 
 import android.content.Context
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.map
+import kotlinx.collections.immutable.toImmutableList
 import javax.inject.Inject
 
 class SourceFactoryConfigRepository @Inject constructor(private val dao: SourceFactoryConfigDao) {
 
     @WorkerThread
-    fun watchAll() = dao.watchAll()
+    fun watchAll() = dao.watchAll().map { it.toImmutableList() }
 
     @WorkerThread
-    suspend fun getAll() = dao.getAll()
+    suspend fun getAll() = dao.getAll().toImmutableList()
 
     @WorkerThread
     suspend fun insertOrReplace(vararg configs: SourceFactoryConfig) = dao.insertOrReplace(*configs)
